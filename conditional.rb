54ce5a9fb783960e5b0ba08c23fd0c90f51981a5
ghalidouga <ghalighalidouga@gmail.com>
2017-05-22 20:17:26 +0800

    tambahan conditional.rb

--- conditional.rb
+++ conditional.rb
@@ -1,20 +1,46 @@
-angka = 21
+# angka = 21
 
-if angka < 10
-    puts "Angka #{angka} adalah angka kecil!"
-elsif angka < 50
-    puts "Angka #{angka} adalah angka sedang!"
+# if angka < 10
+#     puts "Angka #{angka} adalah angka kecil!"
+# elsif angka < 50
+#     puts "Angka #{angka} adalah angka sedang!"
+# else
+#     puts "Angka #{angka} adalah angka besar!"
+# end
+
+# if angka.odd?
+#     puts "Angka #{angka} adalah angka ganjil!"
+# else
+#     puts "Angka #{angka} adalah angka genap!"
+# end
+
+# word = "dank"
+# if word.length == 4
+#     puts "Benar"
+# end
+
+# def ganjil_apa_genap(angka)
+#     if angka.odd?
+#         puts "bilangan ganjil"
+#     else
+#         puts "bilangan genap"
+#     end
+# end
+
+# ganjil_apa_genap(10)
+# ganjil_apa_genap(15)
+# ganjil_apa_genap(17)
+
+jumlah = 10
+x = 8
+y = 2
+buah = "apel"
+enak = true
+
+if jumlah == x + y && buah == "apel"
+    puts "benar"
 else
-    puts "Angka #{angka} adalah angka besar!"
+    puts "salah"
 end
 
-if angka.odd?
-    puts "Angka #{angka} adalah angka ganjil!"
-else
-    puts "Angka #{angka} adalah angka genap!"
-end
-
-word = "dank"
-if word.length == 4
-    puts "Benar"
-end
+puts enak
